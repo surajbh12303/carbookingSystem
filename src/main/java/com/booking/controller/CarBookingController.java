de7fc010de95cbb8d68222f9111985ff0c5a147a
surajbh12303 <72594401+surajbh12303@users.noreply.github.com>
2021-02-05 15:00:15 +0530

    small Responentity change

--- src/main/java/com/booking/controller/CarBookingController.java
+++ src/main/java/com/booking/controller/CarBookingController.java
@@ -33,8 +33,8 @@ public class CarBookingController {
 	}
 
 	@PostMapping("carbookingdetails")
-	public List<Booking> getCarBookingDetails(@RequestBody BookingDto bookingDto) {
-		return this.bookingService.getCarBookingBetDates(bookingDto);
+	public ResponseEntity<List<Booking>> getCarBookingDetails(@RequestBody BookingDto bookingDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(this.bookingService.getCarBookingBetDates(bookingDto));
 	}
 
 }
